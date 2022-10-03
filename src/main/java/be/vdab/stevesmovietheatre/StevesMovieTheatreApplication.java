0837ea858d15829b21e88681f77c3dd1a263a2f2
SVG <steve.vangoethem@vdabcampus.be>
2022-10-03 13:38:40 +0200

    0.1 Creation

---
+++ src/main/java/be/vdab/stevesmovietheatre/StevesMovieTheatreApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.stevesmovietheatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StevesMovieTheatreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StevesMovieTheatreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/stevesmovietheatre/StevesMovieTheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.stevesmovietheatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StevesMovieTheatreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
